cee796e91cbffcb49da454910c0d4ad6c49bf7fd
tataPast <tatyana.pasternak@gmail.com>
2018-07-24 21:36:34 +0300

    LoginTestWithoutPageObject was added

--- PasternakProj/src/main/java/mainPack/App.java
+++ PasternakProj/src/main/java/mainPack/App.java
@@ -1,13 +1,9 @@
 package mainPack;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "777" );
     }
 }
