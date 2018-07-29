21c783315a019ca68fb6192de0aa892e4064012d
Denys <denis809@bigmir.net>
2018-07-30 00:57:00 +0300

    loginTest was added

---
+++ KurishkoProj/src/test/java/loginTests/LoginTest.java
@@ -0,0 +1,28 @@
+package loginTests;
+
+import org.junit.Test;
+import parentTest.ParentTest;
+
+public class LoginTest extends ParentTest {
+    @Test
+    public void  validLogin(){
+        loginPage.openPage();
+        loginPage.enterLogin("Student");
+        loginPage.enterPass("909090");
+        loginPage.clickOnSubmitButton();
+
+        checkAC("Avatar is not present" , homePage.isAvatarPresent(), true);
+
+    }
+//    @Test
+//    public void  unValidLogin(){
+//        loginPage.openPage();
+//        loginPage.enterLogin("Student");
+//        loginPage.enterPass("909090");
+//        loginPage.clickOnSubmitButton();
+//
+//        checkAC("Avatar is not present" , homePage.isAvatarPresent(), true);
+//
+//    }
+
+}
