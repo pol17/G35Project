ec2b0f41c6a02d8eee90e897178650ee616c81b2
pol17 <pol17@ukr.net>
2018-07-31 20:58:31 +0200

    [QA] SparePage added

---
+++ GnatiukProj/src/main/java/pages/SparePage.java
@@ -0,0 +1,9 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SparePage extends ParentPage {
+    public SparePage(WebDriver webDriver) {
+        super(webDriver, "/dictionary/spares");
+    }
+}
