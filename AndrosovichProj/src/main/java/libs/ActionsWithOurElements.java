ab1c36bee3bd744364f8d18fb3ed13b303283611
Androsovich Ivan <ivan.androsovich@gmail.com>
2018-08-02 21:46:28 +0300

    [QA] AddNewSpare implementation 2

--- AndrosovichProj/src/main/java/libs/ActionsWithOurElements.java
+++ AndrosovichProj/src/main/java/libs/ActionsWithOurElements.java
@@ -2,8 +2,12 @@ package libs;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class ActionsWithOurElements {
     WebDriver webDriver;
@@ -48,6 +52,38 @@ public class ActionsWithOurElements {
     private void printErrorAndStopTest(Exception e) {
         logger.error("Cannot work with element " + e);
         Assert.fail("Cannot work with element " + e);
+    }
+
+    public boolean isElementInList(String xPathLocator) {
+        try {
+            List<WebElement> webElementList = webDriver.findElements(By.xpath(xPathLocator));
+            if (webElementList.size() > 0 ){
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    public void clickOnElement(String xPathLocator) {
+        try {
+            WebElement webElement = webDriver.findElement(By.xpath(xPathLocator));
+            clickOnElement(webElement);
+        } catch (Exception e){
+            printErrorAndStopTest(e);
+        }
+    }
+
+    public void selectValueInDD(WebElement dropDownElement, String value) {
+        try {
+            Select select = new Select(dropDownElement);
+            select.selectByValue(value);
+            logger.info(value + " was selected in DropDown");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
     }
 }
 
