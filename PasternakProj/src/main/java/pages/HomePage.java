3dd2f318ad56f7742d0d299976e28f9d802cfd24
tataPast <tatyana.pasternak@gmail.com>
2018-08-05 22:04:00 +0300

    31.07 lessons code

--- PasternakProj/src/main/java/pages/HomePage.java
+++ PasternakProj/src/main/java/pages/HomePage.java
@@ -2,29 +2,27 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class HomePage extends ParentPage{
+public class HomePage extends ParentPage {
+    @FindBy(xpath = ".//*[@alt='Student'and @class='user-image']")
+    private WebElement avatar;
+
     public HomePage(WebDriver webDriver) {
         super(webDriver, "/");
     }
 
-    public boolean isAvatarPresent(){
-        try{
-            return webDriver.findElement(
-                    By.xpath(".//*[@alt='Student'and @class='user-image']"))
-                    .isDisplayed();
-
-        }catch (Exception e){
-            return false;
-        }
+    public boolean isAvatarPresent() {
+        return actionsWithOurElements.isElementDisplay(avatar);
     }
 
-    public boolean isSubmitButtonPresent(){
-        try{
+    public boolean isSubmitButtonPresent() {
+        try {
             return webDriver.findElement(
                     By.xpath(".//button[@type='submit']"))
                     .isDisplayed();
-        } catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
