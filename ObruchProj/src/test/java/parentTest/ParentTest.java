23d382fc8fd954013ea0ae1b30d7002d7fa8dd45
arafatmeister@gmail.com <6ke-hL2-7ys-QAd>
2018-08-14 21:22:59 +0300

    Excell was added

--- ObruchProj/src/test/java/parentTest/ParentTest.java
+++ ObruchProj/src/test/java/parentTest/ParentTest.java
@@ -1,5 +1,7 @@
 package parentTest;
 
+import libs.ConfigProperties;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Assert;
@@ -27,6 +29,7 @@ public class ParentTest {
     protected SparesPage sparesPage;
     protected EditSparePage editSparePage;
     String browser = System.getProperty("browser");
+    protected static ConfigProperties configProperties = ConfigFactory.create(ConfigProperties.class);
 
 
     @Before
