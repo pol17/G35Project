2b4fb59b994f72cca0b10a72946f2ea1d323681a
Olga Voloshchuk <olga.voloshchuk@gmail.com>
2018-08-14 21:39:44 +0300

    [QA] checkbox

--- VoloschukProj/src/main/java/libs/ActionsWithOurElements.java
+++ VoloschukProj/src/main/java/libs/ActionsWithOurElements.java
@@ -111,6 +111,20 @@ public class ActionsWithOurElements {
         }
     }
 
+    public void setNeedeStateToCheckBox(WebElement webElement, String neededState){
+        if ("check".equals(neededState) || "uncheck".equals(neededState)){
+            if (webElement.isSelected() && "check".equals(neededState)) {
+                logger.info("checkbox is already checked");
+            } else if (webElement.isSelected() && "uncheck".equals(neededState)) {
+                clickOnElement(webElement);
+                logger.info("checkbox was unchecked");
+            }
+        } else {
+            logger.error(String.format("%s - is not expected state", neededState));
+            Assert.fail(String.format("%s - is not expected state", neededState));
+        }
+    }
+
     public void clickValueInDD(WebElement dropDownList, WebElement dropDownElement) {
         clickOnElement(dropDownList);
         clickOnElement(dropDownElement);
